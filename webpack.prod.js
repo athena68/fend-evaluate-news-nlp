05c09680f97393b0f5fcb7a032747a09db1a0aca
Tuan <tnguy258@ford.com>
2023-05-25 22:06:26 +0700

    add ServiceWorker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -17,8 +19,8 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -26,6 +28,8 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
